ac7df452ce38c822830caa1e7fde6d0b66929206
Stan Peev <stan.peev@gmail.com>
2015-07-09 22:45:41 +0100

    Changes to design.rb

--- app/models/design.rb
+++ app/models/design.rb
@@ -6,9 +6,9 @@ class Design
   field :title,         type: String
   field :description,   type: String
   field :style,         type: String
-  field :designer_id,   type: Integer
 
   belongs_to :designer
+
   has_mongoid_attached_file :image, styles: { medium: "1000x800#", small: "350x250>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
